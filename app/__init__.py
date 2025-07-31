ade174e0c1d37a18341f26cb71531508e3fa8166
Phyciatrist <benjamingweimer@gmail.com>
2025-07-31 11:34:18 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,7 @@
-
+/ProjectCloudflower/      <-- This is your main GitHub repository folder
+├── .gitignore
+├── README.md
+├── app/                  <-- The folder for your Flask application
+│   ├── __init__.py       <-- CORRECT LOCATION
+│   └── models.py         <-- MOVE THIS HERE
+└── run.py                <-- This file will start your app (we'll create it later)
